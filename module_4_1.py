208a0f97bd389eac6048e3dae8d8205a7579245a
Alexander23081990 <95206213+Alexander23081990@users.noreply.github.com>
2024-10-17 11:19:34 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -1,10 +1,10 @@
-from fake_math import divide as d1
-from true_math import divide as d2
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
 
-result1 = d1(69, 3)
-result2 = d1(3, 0)
-result3 = d2(49, 7)
-result4 = d2(15, 0)
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
 print(result1)
 print(result2)
 print(result3)
